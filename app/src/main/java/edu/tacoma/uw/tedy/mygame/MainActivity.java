3e248767912a8af72237c95917b2df6a73652105
Teddybada <tidoone@yahoo.com>
2017-02-14 16:29:13 -0800

    first commit

---
+++ app/src/main/java/edu/tacoma/uw/tedy/mygame/MainActivity.java
@@ -0,0 +1,210 @@
+package edu.tacoma.uw.tedy.mygame;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.LoginInteractionListener,
+        RegisterFragment.UserAddListener
+{
+    private SharedPreferences mSharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+//            // Create a new Fragment to be placed in the activity layout
+//            LoginFragment firstFragment = new LoginFragment();
+//
+//            // In case this activity was started with special instructions from an
+//            // Intent, pass the Intent's extras to the fragment as arguments
+//            firstFragment.setArguments(getIntent().getExtras());
+//
+//            // Add the fragment to the 'fragment_container' FrameLayout
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.fragment_container, firstFragment).commit();
+
+        }
+
+
+        LoginFragment loginFragment = new LoginFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, loginFragment).commit();
+
+
+    }
+
+    public void register1(View v) {
+
+                RegisterFragment registerFragment = new RegisterFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, registerFragment)
+                        .addToBackStack(null)
+                        .commit();
+//            }
+//        });
+    }
+
+    @Override
+    public void addUser(String url) {
+        AddUserTask task = new AddUserTask();
+        task.execute(new String[]{url.toString()});
+
+//                LoginFragment loginFragment = new LoginFragment();
+//                getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.fragment_container, loginFragment)
+//                        .addToBackStack(null)
+//                        .commit();
+        getSupportFragmentManager().popBackStackImmediate();
+    }
+
+
+    @Override
+    public void login(String userId, String pwd) {
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            //Check if the login and password are valid
+            //new LoginTask().execute(url);
+        }
+        else {
+            Toast.makeText(this, "No network connection available. Cannot authenticate user",
+                    Toast.LENGTH_SHORT) .show();
+            return;
+        }
+
+        // Load info into shared Preferences.
+        mSharedPreferences
+                .edit()
+                .putBoolean(getString(R.string.LOGGEDIN), true)
+                .commit();
+
+        //Store in file
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            //Check if the login and password are valid
+            //new LoginTask().execute(url);
+            try {
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+                        openFileOutput(getString(R.string.LOGIN_FILE)
+                                , Context.MODE_PRIVATE));
+                outputStreamWriter.write("email = " + userId + ";");
+                outputStreamWriter.write("password = " + pwd);
+                outputStreamWriter.close();
+                Toast.makeText(this,"Stored in File Successfully!", Toast.LENGTH_LONG)
+                        .show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        Intent i = new Intent(this, gameActivity.class);
+        startActivity(i);
+        // Will ensure that the user can not return to the sign in screen by pressing the back button.
+        finish();
+
+    }
+
+//    @Override
+//    public void onFragmentInteraction(Uri uri) {
+//
+//    }
+
+    private class AddUserTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to add user, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+                if (status.equals("success")) {
+                    Toast.makeText(getApplicationContext(), "User successfully added!"
+                            , Toast.LENGTH_LONG)
+                            .show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to add: "
+                                    + jsonObject.get("error")
+                            , Toast.LENGTH_LONG)
+                            .show();
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Something wrong with the data" +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/edu/tacoma/uw/tedy/mygame/gameActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.tedy.mygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
